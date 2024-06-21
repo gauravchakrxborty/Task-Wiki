e5aca40b48320a512aa49959033a2bf1ce53a5cc
gaurav <gauravchakrxborty@gmail.com>
2024-06-21 13:36:16 +0530

    Firebase connection Testing

--- app/src/main/java/com/example/task_wiki/MainActivity.kt
+++ app/src/main/java/com/example/task_wiki/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.task_wiki
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.database.core.Tag
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val user: MutableMap<String, Any> = HashMap()
+        user["first"] = "Gaurav"
+        user["last"] = "Chakraborty"
+        user["born"] = 1997
+
+        FirebaseFirestore.getInstance().collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d("TAG", "DocumentSnapShot added with ID: " + documentReference.id)
+            }
+            .addOnFailureListener { e ->
+                Log.w("TAG", "Error adding document", e)
+            }
     }
 }
